b1450a69513c1c900522dacf4b1474eb88dc88fe
Wayne Zhang <wayne.zhang@rdbrck.com>
2017-03-01 14:57:36 -0800

    2017 api

--- app/main.py
+++ app/main.py
@@ -13,34 +13,36 @@ def static(path):
 
 @bottle.get('/')
 def index():
-    head_url = '%s://%s/static/head.png' % (
-        bottle.request.urlparts.scheme,
-        bottle.request.urlparts.netloc
-    )
+    # head_url = '%s://%s/static/head.png' % (
+    #     bottle.request.urlparts.scheme,
+    #     bottle.request.urlparts.netloc
+    # )
 
     return {
-        'color': '#00ff00',
-        'head': head_url
+
     }
 
 @bottle.post('/start')
 def start():
     data = bottle.request.json
-    boardSize["width"] = data["width"]
-    boardSize["height"] = data["height"]
+    boardSize["width"] = data["width"] + 1
+    boardSize["height"] = data["height"] + 1
     return {
-        'taunt': 'Snake master'
+        'head_url': 'http://static4.businessinsider.com/image/56c640526e97c625048b822a-480/donald-trump.jpg',
+        'color': '#FFFF00',
+        'name': 'Wayne',
+        'taunt': "abcdefg"
     }
 
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-    mySnake = filter(lambda x: x["name"] == "Enemy Snake", data["snakes"])[0]
+    mySnake = filter(lambda x: x["name"] == "Wayne", data["snakes"])[0]
     myPos = mySnake["coords"][0]
     if (len(data["food"]) == 0):
         return {
-        'move': 'east',
-        'taunt': 'Snake master'
+        'move': 'right',
+        'taunt': 'right'
         }
     else:
         food = {"coor": data["food"][0], "dis": heuristic(myPos, data["food"][0])}
@@ -61,21 +63,20 @@ def move():
                 board[coords[0]][coords[1]] = 1
                 count = count + 1
         board[food["coor"][0]][food["coor"][1]] = 2
-        
+
         #Build Wall
         for i in range(0, boardSize["width"]):
             board[i][0] = 1
             board[i][boardSize["height"] - 1] = 1
-        
+
         for j in range(0, boardSize["height"]):
             board[0][j] = 1
             board[boardSize["width"] - 1][j] = 1
-        
-        print "My position is ", myPos
+
         path = BFS(myPos[0],myPos[1],board)
         #print path[len(path) - 2]
-        print "Path length", len(path)
-        #print nextDirection(myPos,path[len(path) - 2])
+        print len(path)
+        print nextDirection(myPos,path[len(path) - 2])
 
     return {
     'move': nextDirection(myPos,path[len(path) - 2]),
@@ -87,40 +88,38 @@ def end():
     data = bottle.request.json
 
     return {
-        'taunt': 'GG'
+        'taunt': 'end'
     }
 
 def nextDirection(last, next):
     if (last[0] == next[0]):
         if(next[1] - last[1] < 0):
-            return 'north'
+            return 'up'
         else:
-            return 'south'
+            return 'down'
     elif(last[1] == next[1]):
         if(next[0] - last[0] < 0):
-            return 'west'
+            return 'left'
         else:
-            return 'east'
+            return 'right'
 
 
 def BFS(x,y,Map):
     Map[x][y] = 0
-    queue = deque( [(x,y,None)]) #create queue
-
-    while len(queue)>0: #make sure there are nodes to check left
-        node = queue.popleft() #grab the first node
-        
-        x = node[0] #get x and y
+    queue = deque( [(x,y,None)])
+    while len(queue)>0:
+        node = queue.popleft()
+        x = node[0]
         y = node[1]
         #print "(x , y) is ===> (", x, " , ", y, ")"
-        if (x < boardSize["width"] and y < boardSize["height"]): 
-            if Map[x][y] == 2: #check if it's an exit
-                return GetPathFromNodes(node) #if it is then return the path
-            if (Map[x][y] != 0): #if it's not a path, we can't try this spot
+        if (x < boardSize["width"] and y < boardSize["height"]):
+            if Map[x][y] == 2:
+                return GetPathFromNodes(node)
+            if (Map[x][y] != 0):
                 continue
-            Map[x][y]= -1 #make this spot explored so we don't try again
-            for i in [[x-1,y],[x+1,y],[x,y-1],[x,y+1]]: #new spots to try
-                queue.append((i[0],i[1],node)) #create the new spot, with node as the parent
+            Map[x][y]= -1
+            for i in [[x-1,y],[x+1,y],[x,y-1],[x,y+1]]:
+                queue.append((i[0],i[1],node))
     return []
 
 def GetPathFromNodes(node):
@@ -136,4 +135,4 @@ def heuristic(cell, goal):
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
-    bottle.run(application, host=os.getenv('IP', '127.0.0.1'), port=os.getenv('PORT', '5002'))
+    bottle.run(application, host=os.getenv('IP', '127.0.0.1'), port=os.getenv('PORT', '8080'))
